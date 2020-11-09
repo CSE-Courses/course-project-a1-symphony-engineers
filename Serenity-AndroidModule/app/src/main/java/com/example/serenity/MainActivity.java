c674aa6f836bbd491cecdc34ff5252890c992741
reboot-armani <morshedu@buffalo.edu>
2020-11-09 15:57:29 -0500

    Story #110 Task #114 #116 #113 Added Android Platform

---
+++ Serenity-AndroidModule/app/src/main/java/com/example/serenity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.serenity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://afternoon-spire-76193.herokuapp.com/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
