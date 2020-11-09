01570fded333a98cb532b11c363233c7c6cc0e63
reboot-armani <morshedu@buffalo.edu>
2020-11-09 00:08:25 -0500

    Story #110 Task #114 #116 #113 Added Android Platform

---
+++ Serenity-AndroidModule/app/src/main/java/com/example/serenity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.serenity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www-student.cse.buffalo.edu/CSE442-542/2020-Fall/cse-442f/course-project-a1-symphony-engineers-develop-2/homePage/homePage.html");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
