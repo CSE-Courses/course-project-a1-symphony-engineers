d0691a08334cffac0af3f7d7854fff82e074d9cb
Ab21tm <arameshb@buffalo.edu>
2020-12-07 04:58:14 +0530

    User story #144 routes updated

---
+++ routes/quotes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl;
+        req.flash('error','You must be signed in');
+        return res.redirect('/login');
+    }
+    res.render('quotes');
+});
+
+
+module.exports = router;
