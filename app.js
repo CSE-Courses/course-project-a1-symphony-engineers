042ed63b2dd83166d3f219bfae091f56294dcac8
hahire <36177576+hahire@users.noreply.github.com>
2020-12-07 16:17:51 -0500

    UserStory #145 Task #161 #162 Chatbot Implementation

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const Local = require('passport-local');
 const flash = require('connect-flash');
 const User = require('./models/user');
 
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+const bodyParser = require('body-parser');
+
 const userRoutes = require('./routes/users');  
 const moodcheckRoutes = require('./routes/moodcheck');  
 const meditationRoutes = require('./routes/meditation');
@@ -21,6 +25,8 @@ const yogaContentsRoutes = require('./routes/yogaContents');
 const quizRoutes = require('./routes/quiz');
 const warningRoutes = require('./routes/warning');
 const resultsRoutes = require('./routes/results');
+const quotesRoutes = require('./routes/quotes');
+const needhelpRoutes = require('./routes/needhelp');
 const { MongoStore } = require('connect-mongo');
 
 const MongoDBStore = require("connect-mongo")(session);
@@ -104,8 +110,8 @@ app.use('/yogaContents',yogaContentsRoutes);
 app.use('/quiz',quizRoutes);
 app.use('/warning',warningRoutes);
 app.use('/results',resultsRoutes);
-
-
+app.use('/quotes',quotesRoutes);
+app.use('/needhelp',needhelpRoutes);
 
 app.get('/',(req,res)=>{
     res.render('home')
@@ -121,3 +127,72 @@ const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`Serving on port ${port}`)
 });
+
+
+
+const sessionId = uuid.v4();
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post('/send-msg', (req,res)=>{
+    runSample(req.body.MSG).then(data=>{
+      res.send({Reply:data})
+    })
+})
+
+app.get('/chat-bot',(req,res)=>{
+  res.render('index')
+});
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'helperbot-9tgk') {
+
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      keyFilename:"helperbot-9tgk-9352a90c3d36.json"
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: msg,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+  return result.fulfillmentText;
+}
+
